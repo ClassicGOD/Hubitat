7b4b461c29e2e5f5de05b68b1b2d0019165b46c8
ClassicGOD <classicgod@gmail.com>
2020-05-24 16:44:02 +0200

    Create  fibaro-double-switch-2-child-device.groovy

---
+++ Drivers/fibaro-double-switch-2.src/fibaro-double-switch-2-child-device.groovy
@@ -0,0 +1,25 @@
+/**
+ *  Fibaro Double Switch 2 Child Device
+ *
+ *  Copyright 2020 Artur Draga
+ *
+ */
+metadata {
+	definition (name: "Fibaro Double Switch 2 Child Device", namespace: "ClassicGOD", author: "Artur Draga") {
+		capability "Switch"
+		capability "Energy Meter"
+		capability "Power Meter"
+
+		command "toggle"
+	}
+}
+
+def on() { parent.childOn() }
+
+def off() {	parent.childOff() }
+
+def toggle() { parent.childToggle() }
+
+//def reset() { parent.childReset() }
+
+//def refresh() { parent.childRefresh() }
